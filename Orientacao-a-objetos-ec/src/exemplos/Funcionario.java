5d1643d4d75fd0e7f5331b023b719cd880599641
Marcio <mar_geo_jp@yahoo.com.br>
2014-05-05 21:43:50 -0300

    Adição dos metodos get e set

--- Orientacao-a-objetos-ec/src/exemplos/Funcionario.java
+++ Orientacao-a-objetos-ec/src/exemplos/Funcionario.java
@@ -1,40 +1,63 @@
 package exemplos;
 
-
 public class Funcionario {
 	private String nome;
 	private int ID;
 	private double salario;
-	 
-	 
-	public Funcionario(String nome, double salario, int ID){
+
+	public Funcionario(String nome, double salario, int ID) {
 		System.out.println(this);
-		this.nome = nome;
-		this.ID = ID;
-		this.salario = salario;
-	}	
-	
-	public Funcionario(){
+		setNome(nome);
+		setID(ID);
+		setSalario(salario);
+	}
+
+	public Funcionario() {
 		System.out.println("Método construtor padrão invocado.");
 	}
-	
-	public String toString(){
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String toString() {
 		return "Foi usado o: System.out.println(this)";
 	}
-	
-	public void exibir(){
-		System.out.printf("O funcionario %s, de número %d, recebe %.2f por mes", this.nome, this.ID, this.salario);
+
+	public void exibir() {
+		System.out.printf(
+				"O funcionario %s, de número %d, recebe %.2f por mes",
+				this.nome, this.ID, this.salario);
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		String nome = "Márcio";
 		int ID = 123;
 		double salario = 5000;
-		
+
 		Funcionario f1 = new Funcionario(nome, salario, ID);
 		f1.exibir();
-		
-		
+
 	}
-	
+
 }
