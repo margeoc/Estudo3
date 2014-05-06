3040ecc123e693fd657867c89dec2761aca5c687
Marcio <mar_geo_jp@yahoo.com.br>
2014-05-05 21:18:13 -0300

    commit teste

---
+++ Orientacao-a-objetos-ec/src/exemplos/Funcionario.java
@@ -0,0 +1,40 @@
+package exemplos;
+
+
+public class Funcionario {
+	private String nome;
+	private int ID;
+	private double salario;
+	 
+	 
+	public Funcionario(String nome, double salario, int ID){
+		System.out.println(this);
+		this.nome = nome;
+		this.ID = ID;
+		this.salario = salario;
+	}	
+	
+	public Funcionario(){
+		System.out.println("Método construtor padrão invocado.");
+	}
+	
+	public String toString(){
+		return "Foi usado o: System.out.println(this)";
+	}
+	
+	public void exibir(){
+		System.out.printf("O funcionario %s, de número %d, recebe %.2f por mes", this.nome, this.ID, this.salario);
+	}
+	
+	public static void main(String[] args){
+		String nome = "Márcio";
+		int ID = 123;
+		double salario = 5000;
+		
+		Funcionario f1 = new Funcionario(nome, salario, ID);
+		f1.exibir();
+		
+		
+	}
+	
+}
